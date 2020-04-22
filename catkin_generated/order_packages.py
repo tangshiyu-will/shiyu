142649748b8b086b6c3f4f86a91b679e69eed255
tangshiyu-will <54060334+tangshiyu-will@users.noreply.github.com>
2020-04-21 23:58:03 -0700

    Add files via upload

---
+++ catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/tang/catkin_ws_new/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/tang/backup2/devel;/home/tang/test_ws/devel;/opt/ros/kinetic".split(';') if "/home/tang/backup2/devel;/home/tang/test_ws/devel;/opt/ros/kinetic" != "" else []
